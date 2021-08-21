7f628e36b66551efc69acf4a757bee1a5f9e718d
cuadriante <adriana@sinapsis.ws>
2021-08-21 17:21:57 -0600

    add zeroes to destiny file

--- FileExtraction.cpp
+++ FileExtraction.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 ofstream* g_destFile;
 vector<int> g_intVector;
+int g_highestNumber;
+int g_highestNumberDigits;
 
 void FileExtraction::openAndExtractFile(string initialFileName, string destinyFileName) {
     ifstream initialFile;
@@ -24,7 +26,8 @@ void FileExtraction::openAndExtractFile(string initialFileName, string destinyFi
     int i = 0;
     if (initialFile.is_open() && destFile.is_open())
     {
-        getHighestNumber(&initialFile);
+        g_highestNumber = getHighestNumber(&initialFile);
+        g_highestNumberDigits = getDigits(g_highestNumber);
         initialFile.open(initialFileName);
         std::cout << "initialFile opened \n";
         while ( getline (initialFile, line) )
@@ -46,14 +49,26 @@ void FileExtraction::parseToDestinyFile(string line, ofstream* destinyFile) {
     size_t pos = 0;
     std::string token;
     int *arr;
+
+
     while ((pos = line.find(delimiter)) != std::string::npos) {
         token = line.substr(0, pos);
 
         int intToken = stoi(token);
 
-        std::cout << "parsed: " << intToken << std::endl;
+        int intTokenDigits = getDigits(intToken);
+
+        int quantityToAdd = g_highestNumberDigits - intTokenDigits;
+        while (quantityToAdd > 0)
+        {
+            *destinyFile << "0";
+            quantityToAdd --;
+        }
+        //addZeroes(intToken, destinyFile);
 
         *destinyFile << intToken << ",";
+
+        std::cout << "parsed: " << intToken << std::endl;
 
         g_intVector.insert(g_intVector.cend(), intToken);
 
@@ -103,4 +118,26 @@ int FileExtraction::getHighestNumber(std::ifstream* initialFile) {
 
     return highestNumber;
 }
+
+int FileExtraction::addZeroes(int intToken, ofstream* destinyFile) {
+    string toAdd;
+    int quantityToAdd = getDigits(g_highestNumber) - getDigits(intToken);
+    while (quantityToAdd > 0)
+    {
+        *destinyFile << "0";
+        quantityToAdd --;
+    }
+
+    return 0;
+}
+
+int FileExtraction::getDigits(int intToken) {
+    int digits = 0;
+    while(intToken != 0) {
+        intToken = intToken / 10;
+        digits++;
+    }
+    return digits;
+}
+
 
